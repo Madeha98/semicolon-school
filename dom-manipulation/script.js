978439f9722daec6ea692456c93b3a4f8dcd6681
Madeha98 <madeha555@gmail.com>
2024-09-25 17:40:47 +0100

    added changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,13 +1,30 @@
+//  1. Update the `addTask` function to:
+
 function addTask() {
   var input = document.getElementById("taskInput");
   var list = document.getElementById("taskList");
   var newItem = document.createElement("li");
-  newItem.innerHTML = input.value;
-  list.appendChild(newItem);
-  input.value = "";
+  // const button = document.createElement("button");
+  // completeBtn = document.getElementsByClassName("complete-btn");
+
+  //  a. Check if the input is empty before adding a task
+  if (input.value !== "") {
+    newItem.appendChild(document.createTextNode(input.value)); // b. Create a text node instead of using innerHTML
+    list.appendChild(newItem);
+    // newItem.innerHTML = `${input.value} <button>Completed</button> <button>Delete</button>`; // c. Add a delete button next to each new tas
+    // newItem.appendChild(button).classList.add("complete-btn");
+    // completeBtn.innerHTML = `<button>Complete</button>`;
+    input.value = "";
+  }
 }
 
-// This function is not used yet
+var deleteBtn = document.createElement(`button`);
+deleteBtn.classList.add("delete-btn");
+
+// 2. Implement the `completeTask` function:
+
 function completeTask(task) {
   task.classList.add("completed");
 }
+
+completeTask();
