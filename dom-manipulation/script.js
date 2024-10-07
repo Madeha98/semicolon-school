1206883fc50c7e13400b5a1dab096ddee1715ff9
Madeha98 <madeha555@gmail.com>
2024-10-07 23:10:00 +0100

    completed functions

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,30 +1,41 @@
-//  1. Update the `addTask` function to:
-
 function addTask() {
   var input = document.getElementById("taskInput");
   var list = document.getElementById("taskList");
   var newItem = document.createElement("li");
-  // const button = document.createElement("button");
-  // completeBtn = document.getElementsByClassName("complete-btn");
 
-  //  a. Check if the input is empty before adding a task
+  // Check if the input is empty before adding a task:
   if (input.value !== "") {
-    newItem.appendChild(document.createTextNode(input.value)); // b. Create a text node instead of using innerHTML
+    newItem.appendChild(document.createTextNode(input.value)); // Create a text node instead of using innerHTML
     list.appendChild(newItem);
-    // newItem.innerHTML = `${input.value} <button>Completed</button> <button>Delete</button>`; // c. Add a delete button next to each new tas
-    // newItem.appendChild(button).classList.add("complete-btn");
-    // completeBtn.innerHTML = `<button>Complete</button>`;
+    newItem.addEventListener("click", function () {
+      completeTask(newItem);
+    });
+    var deleteBtn = document.createElement("button"); // Add a delete button next to each new task
+    deleteBtn.textContent = `Delete`;
+    deleteBtn.classList.add("delete-btn");
+    newItem.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", deleteTask); // Make it listen to the delete button for each task
     input.value = "";
   }
 }
 
-var deleteBtn = document.createElement(`button`);
-deleteBtn.classList.add("delete-btn");
-
-// 2. Implement the `completeTask` function:
-
+// Implement the `completeTask` function:
+var tasks = document.querySelectorAll("li");
+var button = document.createElement("button");
 function completeTask(task) {
-  task.classList.add("completed");
+  task.classList.toggle(`completed`); // It should toggle the ‘completed’ class on and off
 }
+tasks.forEach((task) => {
+  // Make it listen to a click event on each task
+  task.addEventListener("click", function () {
+    completeTask(task);
+  });
+});
 
-completeTask();
+console.log(document.querySelectorAll("li"));
+
+// Create a `deleteTask` function:
+var deleteBtn = document.getElementsByClassName("delete-btn");
+function deleteTask(task) {
+  task.target.parentElement.remove(); // It should remove the task from the list
+}
